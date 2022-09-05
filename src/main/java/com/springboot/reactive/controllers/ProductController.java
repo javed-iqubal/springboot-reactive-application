a0d290d1a67e80d827d067bc316bda95c61232bd
javed-iqubal <jiqbal07@gmail.com>
2022-09-05 22:49:35 +0530

    add post and get method

--- src/main/java/com/springboot/reactive/controllers/ProductController.java
+++ src/main/java/com/springboot/reactive/controllers/ProductController.java
@@ -1,4 +1,32 @@
 package com.springboot.reactive.controllers;
 
+import com.springboot.reactive.dto.ProductDto;
+import com.springboot.reactive.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(ProductController.ROOT_PATH)
 public class ProductController {
+
+    public static final String ROOT_PATH = "/v1/products";
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("all")
+    public Flux<ProductDto> getProducts(){
+        return productService.getProducts();
+    }
+
+    @GetMapping("{id}")
+    public Mono<ProductDto> getProductById(@PathVariable("id") String productId){
+        return productService.getProduct(productId);
+    }
+
+    @PostMapping
+    public Mono<ProductDto> getProductById(@RequestBody ProductDto productDto){
+        return productService.saveProduct(Mono.just(productDto));
+    }
 }
